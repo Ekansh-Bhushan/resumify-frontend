db385658855070b91f30c8b4993b9a921b24e05b
Ekansh <ekanshbhushan2k22@gmail.com>
2023-11-10 22:12:27 +0530

    added some more pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Signup from '../src/Components/signinpage/signup.jsx';
 import Login from './Components/signinpage/login.jsx';
 import Mainpage from './Components/mainpage/mainpage.js';
 import PlatformfeedbackForm from './Components/PlatfromFeedback/PlatformfeedbackForm.js';
+import ChangePasswordPage from './Components/passwordChange/changepassword.js';
+import ResetPasswordEmail from './Components/resetpassword/resetpasswordEmail.js';
+import ResetPasswordPage from './Components/resetpassword/resetpassword.js';
 // import Temp from './Components/templates-page/temppage.jsx'
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
         <Route path="/login" element = {<Login/>} />
         <Route path="/" element= {<Mainpage/>} />
         <Route path="/feedback" element = {<PlatformfeedbackForm/>}/>
+        <Route path='/changePassword' elemen={<ChangePasswordPage/>}/>
+        <Route path='/emailrequest' element={<ResetPasswordEmail/>}/>
+        <Route path='/resetpassword' element = {<ResetPasswordPage/>}/>
         {/* <Route path="/template" element ={<Temp/>}/> */}
       </Routes>
     </Router>
