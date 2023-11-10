02907e7f56d4efc0d928e37b233a44291aaab64f
Kartik <kartikbindra_23cs211@dtu.ac.in>
2023-11-10 23:51:24 +0530

    improved template page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { BrowserRouter as Router, Route, Link, Switch, Routes } from 'react-rout
 import Signup from '../src/Components/signinpage/signup.jsx';
 import Login from './Components/signinpage/login.jsx';
 import Mainpage from './Components/mainpage/mainpage.js';
-// import Temp from './Components/templates-page/temppage.jsx'
+import Temp from './Components/templates-page/temppage.jsx'
 
 function App() {
   return (
     <>
-<Router>
-      <Routes>
-        <Route path="/signup" element = {<Signup/>} />
-        <Route path="/login" element = {<Login/>} />
-        <Route path="/" element= {<Mainpage/>} />
-        {/* <Route path="/template" element ={<Temp/>}/> */}
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/signup" element = {<Signup/>} />
+          <Route path="/login" element = {<Login/>} />
+          <Route path="/" element= {<Mainpage/>} />
+          <Route path="/template" element ={<Temp/>}/>
+        </Routes>
+      </Router>
+      {/* <temp/> */}
     </>
   )
 }
--- src/Components/templates-page/temppage.jsx
+++ src/Components/templates-page/temppage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './temppage.css';
-// import Img from './src/background.png'
-
+import logo from '../../logo.svg';
+import company from '../../Components/images/comapny-specific.png';
+import minimal from '../../Components/images/minimal.png';
+import modern from '../../Components/images/modern.png';
+import professional from '../../Components/images/professional.png';
 
 function temp() {
     return (
         <div className='background'>
             <nav className="navbar">
                 <div className="left">
-                    <img src="/" alt="Logo" className="logo" />
+                    <img src= {logo} alt="Logo" className="logo" />
                 </div>
                 <div className="right">
                     <ul>
@@ -20,29 +23,46 @@ function temp() {
                 </div>
             </nav>
             <div className="text">
-                Ready-to-Edit Resume Templates
+                Ready-to-Edit Resume <br/>Templates
             </div>
             <div className="text2">
                 Resume templates that can help you land in your dream company.
             </div>
-            <div className="container">
-                <div className="row">
-                    <a href='/' className='links'>
-                        Professional Templates
 
-                    </a>
-
-                    <a href='/' className='links'>
-                        Company Specific Templates
-                    </a>
+            <div className='button'>
+                    <button className= "create_resume">
+                        Create My Resume
+                    </button>
                 </div>
-                <div className="row">
-                    <a href='/' className='links'>
-                        Modern Templates
+            <div className="container">
+                <div className="column">
+                    <div className='templates'>
+                        <img src={company} alt="company-specific" className='icons'/>
+                        <a href='/' className='links'>
+                            Professional Templates
                         </a>
-                    <a href='/' className='links'>
-                        Minimal Templates
-                    </a>
+                    </div>
+                    <div className='templates'>
+                        <img src={modern} alt="modern" className='icons'/>
+                        <a href='/' className='links'>
+                            Modern Templates
+                        </a>
+                    </div>
+                </div>
+                
+                <div className="column">
+                    <div className='templates'>
+                        <img src={minimal} alt="minimal" className='icons'/>
+                        <a href='/' className='links'>
+                            Company Specific Templates
+                        </a>
+                    </div>
+                    <div className='templates'>
+                        <img src={professional} alt="professional" className='icons'/>
+                        <a href='/' className='links'>
+                            Minimal Templates
+                        </a>
+                    </div>
                 </div>
             </div>
             {/* <div className='icon'> */}
