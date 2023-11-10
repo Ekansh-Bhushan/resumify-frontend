c150f2ad5faa6fe722300a9241ca3b590e46c5a0
Ekansh <ekanshbhushan2k22@gmail.com>
2023-11-10 21:18:16 +0530

    topbar to main page

--- src/Components/mainpage/mainpage.js
+++ src/Components/mainpage/mainpage.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Topbar from '../topbar/topbar'
 
 function mainpage() {
   return (
+    <>
+    <Topbar/>
     <div>
       hello world
     </div>
+    </>
   )
 }
 
