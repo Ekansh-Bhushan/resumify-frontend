9bcb2982a560f497104ebb4e7d631d6f25c8373d
Ekansh <ekanshbhushan2k22@gmail.com>
2023-11-12 20:58:53 +0530

    added something to mainpage

--- src/Components/tempedit/tempedit.jsx
+++ src/Components/tempedit/tempedit.jsx
@@ -1,17 +1,1 @@
-import React from 'react'
-import "./tempedit.css"
-// import Img from './src/background.png'
 
-function tempedit() {
-
-
-    return (
-        <div className='background'>
-            <div>
-                hello
-            </div>
-        </div>
-    )
-}
-
-export default tempedit
--- src/Components/topbar/topbar.js
+++ src/Components/topbar/topbar.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import "./topbar.css"
-
+import logo from "../images/synergy.png"
 function topbar() {
 
 
   return (
+    <>
+    <nav className='topbar'>
+      <div>
+        {/* logo here */}
+      <img src={logo} alt="" />
+      </div>
+      <div className='menu-list'>
+        <li>
+          Build Your Resume
+        </li>
+        <li>
+          Tutorial
+        </li>
+        <li>
+          Contact Us
+        </li>
+        <li>
+          My Account 
+        </li>
+      </div>
+      <div className='menu-list'>
+        <li>
+          Login IN 
+        </li>
+        <li>
+          Sign IN
+        </li>
+      </div>
+    </nav>
     <div className='logo'>
       Hey i am the top bar 
     </div>
+    </>
   )
 }
 
