bb590017312eeb25cca7f27198ab0e0229042c86
Maanit <maanitarora1234@gmail.com>
2023-11-16 01:27:02 +0530

    minor changes

--- src/Components/mainpage/mainpage.js
+++ src/Components/mainpage/mainpage.js
@@ -1,31 +1,36 @@
-import React from 'react'
-import Topbar from '../topbar/topbar'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon
+import Topbar from '../topbar/topbar';
+
 function mainpage() {
   return (
     <>
-    <Topbar/>
-    <section id="header" className="">
-     <div className="row">
-      <div className="col-10 max-auto">
-       <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
-         <h1>
-          BUILDING YOUR RESUME HAS NEVER BEEN MORE EASIER 
-          <FontAwesomeIcon icon="fa-solid fa-wand-magic-sparkles" />
-         </h1>
-         <h2 className="my-3">
-            Custom Resumes for many companies and more! to differentiate you from the crowd.
-        </h2>
-        <div className="mt-3"> 
-         <a href="" className="btn-get-started">
-          Create My Resume 
-         </a>
+      <Topbar />
+      <section id="header" className="">
+        <div className="row">
+          <div className="col-10 max-auto">
+            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+              <h1>
+                BUILDING YOUR RESUME HAS NEVER BEEN MORE EASIER{' '}
+                <FontAwesomeIcon icon="fa-wand-magic-sparkles" />
+              </h1>
+              <h2 className="my-3">
+                Custom Resumes for many companies and more! to differentiate you from the crowd.
+              </h2>
+              <div className="mt-3">
+                <a href="" className="btn-get-started">
+                  Create My Resume
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-6 order-1 order-lg-2 header-img">
+              <img src="path/to/your/image.jpg" className="img-fluid animated" alt="Description" />
+            </div>
+          </div>
         </div>
-       </div>
-<div className="col-lg-6 order-1 order-lg-2 header-img">
-      <img src={} className="img-fluid aimated"
-    </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default mainpage
+export default mainpage;
